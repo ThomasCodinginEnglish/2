89421890d33ac454e8f7aee486516513a478fbc6
Thomas Coding <thomascodinginenglish@gmail.com>
2025-04-17 17:09:34 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,11 @@
-basic.forever(function () {
-    if (input.lightLevel() < 100) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . # . # .
-            . # . # .
-            `)
+let random = 0
+input.onGesture(Gesture.Shake, function () {
+    random = randint(1, 3)
+    if (random == 1) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (random == 2) {
+        basic.showIcon(IconNames.Square)
     } else {
-        basic.clearScreen()
+        basic.showIcon(IconNames.Scissors)
     }
 })
